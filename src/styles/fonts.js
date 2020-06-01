699c712a2dd4784e010a89f77e4e79b80b426982
Madhav Sikka <madhavsikka99@gmail.com>
2020-06-01 14:58:48 +0530

    update netlify

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,37 +1,37 @@
 import { css } from "styled-components";
 
-// import Montserrat_300_WOFF from "../fonts/Montserrat/montserrat-v14-latin-300.woff";
-// import Montserrat_300_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-300.woff2";
-// import Montserrat_300_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff";
-// import Montserrat_300_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff2";
+import Montserrat_300_WOFF from "../fonts/Montserrat/montserrat-v14-latin-300.woff";
+import Montserrat_300_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-300.woff2";
+import Montserrat_300_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff";
+import Montserrat_300_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff2";
 import Montserrat_400_WOFF from "../fonts/Montserrat/montserrat-v14-latin-regular.woff";
 import Montserrat_400_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-regular.woff2";
-// import Montserrat_400_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-italic.woff";
-// import Montserrat_400_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-italic.woff2";
-// import Montserrat_500_WOFF from "../fonts/Montserrat/montserrat-v14-latin-500.woff";
-// import Montserrat_500_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-500.woff2";
+import Montserrat_400_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-italic.woff";
+import Montserrat_400_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-italic.woff2";
+import Montserrat_500_WOFF from "../fonts/Montserrat/montserrat-v14-latin-500.woff";
+import Montserrat_500_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-500.woff2";
 import Montserrat_700_WOFF from "../fonts/Montserrat/montserrat-v14-latin-700.woff";
 import Montserrat_700_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-700.woff2";
 import Nexa_Light_WOFF from "../fonts/Nexa/nexa-light-webfont.woff";
 import Nexa_Light_WOFF2 from "../fonts/Nexa/nexa-light-webfont.woff2";
 
 const FontFaces = css`
-	${'' /* @font-face {
+	@font-face {
 		font-family: "Montserrat";
 		src: url(${Montserrat_300_WOFF}) format("woff"),
 			url(${Montserrat_300_WOFF2}) format("woff2");
 		font-weight: 300;
 		font-style: normal;
 		font-display: optional;
-	} */}
-	${'' /* @font-face {
+	}
+	@font-face {
 		font-family: Montserrat;
 		src: url(${Montserrat_300_italic_WOFF}) format("woff"),
 			url(${Montserrat_300_italic_WOFF2}), format("woff2");
 		font-weight: 300;
 		font-style: italic;
 		font-display: optional;
-	} */}
+	}
 	@font-face {
 		font-family: "Montserrat";
 		src: url(${Montserrat_400_WOFF}) format("woff"),
@@ -40,7 +40,7 @@ const FontFaces = css`
 		font-style: normal;
 		font-display: optional;
 	}
-	${'' /* @font-face {
+	@font-face {
 		font-family: Montserrat;
 		src: url(${Montserrat_400_italic_WOFF}) format("woff"),
 			url(${Montserrat_400_italic_WOFF2}), format("woff2");
@@ -55,7 +55,7 @@ const FontFaces = css`
 		font-weight: 500;
 		font-style: normal;
 		font-display: optional;
-	} */}
+	}
 	@font-face {
 		font-family: "Montserrat";
 		src: url(${Montserrat_700_WOFF}) format("woff"),
