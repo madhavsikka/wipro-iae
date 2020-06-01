ab1db98eef5a4b2388b12702752b8dcc1f19e6f9
Madhav Sikka <madhavsikka99@gmail.com>
2020-06-01 15:20:49 +0530

    fix font issue

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -2,12 +2,12 @@ import { css } from "styled-components";
 
 import Montserrat_300_WOFF from "../fonts/Montserrat/montserrat-v14-latin-300.woff";
 import Montserrat_300_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-300.woff2";
-import Montserrat_300_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff";
-import Montserrat_300_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff2";
+// import Montserrat_300_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff";
+// import Montserrat_300_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-300italic.woff2";
 import Montserrat_400_WOFF from "../fonts/Montserrat/montserrat-v14-latin-regular.woff";
 import Montserrat_400_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-regular.woff2";
-import Montserrat_400_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-italic.woff";
-import Montserrat_400_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-italic.woff2";
+// import Montserrat_400_italic_WOFF from "../fonts/Montserrat/montserrat-v14-latin-italic.woff";
+// import Montserrat_400_italic_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-italic.woff2";
 import Montserrat_500_WOFF from "../fonts/Montserrat/montserrat-v14-latin-500.woff";
 import Montserrat_500_WOFF2 from "../fonts/Montserrat/montserrat-v14-latin-500.woff2";
 import Montserrat_700_WOFF from "../fonts/Montserrat/montserrat-v14-latin-700.woff";
@@ -22,39 +22,39 @@ const FontFaces = css`
 			url(${Montserrat_300_WOFF2}) format("woff2");
 		font-weight: 300;
 		font-style: normal;
-		font-display: optional;
+		font-display: blocked;
 	}
-	@font-face {
+	${'' /* @font-face {
 		font-family: Montserrat;
 		src: url(${Montserrat_300_italic_WOFF}) format("woff"),
 			url(${Montserrat_300_italic_WOFF2}), format("woff2");
 		font-weight: 300;
 		font-style: italic;
-		font-display: optional;
-	}
+		font-display: blocked;
+	} */}
 	@font-face {
 		font-family: "Montserrat";
 		src: url(${Montserrat_400_WOFF}) format("woff"),
 			url(${Montserrat_400_WOFF2}) format("woff2");
 		font-weight: 400;
 		font-style: normal;
-		font-display: optional;
+		font-display: blocked;
 	}
-	@font-face {
+	${'' /* @font-face {
 		font-family: Montserrat;
 		src: url(${Montserrat_400_italic_WOFF}) format("woff"),
 			url(${Montserrat_400_italic_WOFF2}), format("woff2");
 		font-weight: 400;
 		font-style: italic;
-		font-display: optional;
-	}
+		font-display: blocked;
+	} */}
 	@font-face {
 		font-family: "Montserrat";
 		src: url(${Montserrat_500_WOFF}) format("woff"),
 			url(${Montserrat_500_WOFF2}) format("woff2");
 		font-weight: 500;
 		font-style: normal;
-		font-display: optional;
+		font-display: blocked;
 	}
 	@font-face {
 		font-family: "Montserrat";
@@ -62,7 +62,7 @@ const FontFaces = css`
 			url(${Montserrat_700_WOFF2}) format("woff2");
 		font-weight: 700;
 		font-style: normal;
-		font-display: optional;
+		font-display: blocked;
 	}
 	@font-face {
 		font-family: "Nexa";
@@ -70,7 +70,7 @@ const FontFaces = css`
 			url(${Nexa_Light_WOFF2}) format("woff2");
 		font-weight: 400;
 		font-style: normal;
-		font-display: optional;
+		font-display: blocked;
 	}
 `;
 
