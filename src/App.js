4b2a103df2827063e353a7d3d0aabb7e8213d5ee
Git-bot9000 <amandeep09020@gmail.com>
2020-06-08 11:40:51 +0530

    Dummy About page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import About from "./components/About";
 import ExamDashboard from "./components/ExamDashboard";
 import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -20,6 +21,12 @@ function App() {
 						<Route path="/register">
 							<Register />
 						</Route>
+						
+						<Route path="/about">
+							<About />
+						</Route>
+						
+						
 						<Route path="/exam-dashboard">
 							<ExamDashboard />
 						</Route>
