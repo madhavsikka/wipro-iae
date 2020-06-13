ea35634ffc29cf6a570fb26c4ffb0e8750a7b090
madhavsikka <madhavsikka99@gmail.com>
2020-06-13 14:46:45 +0530

    remove selected option state

--- src/components/Exam/ExamDashboard/Question/QuestionBox.js
+++ src/components/Exam/ExamDashboard/Question/QuestionBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import theme from "../../../../styles/theme";
 import config from "../../../../config";
